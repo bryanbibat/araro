66eb51063ea10a9107910b9f805d5505d3ef15e6
Bryan Bibat <bry@bryanbibat.net>
2012-11-10 17:43:14 +0800

    Add game proper

---
+++ app/controllers/games_controller.rb
@@ -0,0 +1,5 @@
+class GamesController < ApplicationController
+  before_filter :authenticate_user!
+  def show
+  end
+end
--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,10 @@
 Bigas::Application.routes.draw do
+  resource :game, path: "g", only: :show do
+    collection do
+      post :next_day
+    end
+  end
+
   devise_for :users
 
   root to: "home#index"
