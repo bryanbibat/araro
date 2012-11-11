212382c76184e42e890c38081fb31d94c52f53ad
Bryan Bibat <bry@bryanbibat.net>
2012-11-11 09:32:14 +0800

    fixdrought

--- app/models/variety.rb
+++ app/models/variety.rb
@@ -1,5 +1,5 @@
 class Variety < ActiveRecord::Base
-  attr_accessible :acceptability_cooked, :acceptability_raw, :amylose_content, :ave_yield, :blast_rating, :blb_rating, :bph_rating, :breeding_institution_id, :brown_rice, :chalky_grains, :common_name, :glh_rating, :grain_length, :grain_shape, :gt_score, :head_rice, :height, :immature_grains, :line_designation, :maturity, :max_yield, :milling_recovery, :name, :protein, :shb_rating, :stemborer_rating, :tungro_rating, :year_approved, :buy_price, :sell_price, :heat_rating, :salinity_rating, :draught_rating, :flood_rating
+  attr_accessible :acceptability_cooked, :acceptability_raw, :amylose_content, :ave_yield, :blast_rating, :blb_rating, :bph_rating, :breeding_institution_id, :brown_rice, :chalky_grains, :common_name, :glh_rating, :grain_length, :grain_shape, :gt_score, :head_rice, :height, :immature_grains, :line_designation, :maturity, :max_yield, :milling_recovery, :name, :protein, :shb_rating, :stemborer_rating, :tungro_rating, :year_approved, :buy_price, :sell_price, :heat_rating, :salinity_rating, :drought_rating, :flood_rating
 
   belongs_to :breeding_institution
 
