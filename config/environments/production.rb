c1c41c89fbd71e496911ec55db6fd38d62eb5755
Bryan Bibat <bry@bryanbibat.net>
2012-11-12 09:21:24 +0800

    Try sendmail fix

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -50,7 +50,12 @@ Bigas::Application.configure do
 
   # Disable delivery errors, bad email addresses will be ignored
   # config.action_mailer.raise_delivery_errors = false
-
+  config.action_mailer.delivery_method = :sendmail
+  config.action_mailer.sendmail_settings = {
+    :location  => '/usr/sbin/sendmail',
+    :arguments => '-i -t',
+    :openssl_verify_mode  => 'none'
+  } 
   # Enable threaded mode
   # config.threadsafe!
 
