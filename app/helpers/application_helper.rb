d98deb010874f82d3f77809c775fe5b6eb46734f
Bryan Bibat <bry@bryanbibat.net>
2012-11-12 09:35:48 +0800

    Adjust top player image

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -3,6 +3,6 @@ module ApplicationHelper
   def gravatar_for(user)
     gravatar_id = Digest::MD5::hexdigest(user.email.downcase)
     gravatar_url = "http://gravatar.com/avatar/#{gravatar_id}.png?s=32"
-    image_tag(gravatar_url, :alt => user.email, :class => 'gravatar')
+    image_tag(gravatar_url, :class => 'gravatar')
   end
 end
